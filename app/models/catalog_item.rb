3889794a94fc3a90a1087eb8cc6a57bf048ba943
Oleg Petrushchenko <obp2000@mail.ru>
2011-04-01 23:10:36 +0400

    01

--- app/models/catalog_item.rb
+++ app/models/catalog_item.rb
@@ -3,7 +3,7 @@ class CatalogItem < Item
 
   class_inheritable_accessor :season_icon, :season_name
   self.season_icon = AllSeasonsImage
-  self.season_name = human_attribute_name( :season_name1 )
+  self.season_name = I18n.t( :all_seasons )
 
   self.paginate_options = { :per_page => 8 }
   self.js_for_show = []
